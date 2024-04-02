abdbe43309aa32902eeb41b93820f4fd1ea49cec
Aki Peltokangas <v0peak00@students.oamk.fi>
2024-04-02 15:52:07 +0300

    refactor: Implement ISceneSelector interface
    - Add namespace
    - Add enums for scene names
    - Add error logging on scene not found and when trying
        to load same scene.

--- Assets/Scripts/SceneManager.cs
+++ Assets/Scripts/SceneManager.cs
@@ -1,80 +1,70 @@
 using UnityEngine;
-using System.Threading.Tasks;
-using System;
 
-public class SceneManager : MonoBehaviour, ISceneManager
+namespace SceneSelector
 {
-    // Singleton instance
-    public static SceneManager Instance { get; private set; }
-
-    // Events
-    public event Action<string> SceneLoaded;
-    public event Action<string> SceneUnloaded;
-    
-    private void Awake()
+    public class SceneManager : MonoBehaviour, ISceneSelector
     {
-        // If the singleton hasn't been initialized yet
-        if (Instance == null)
+        public enum SceneName
         {
-            Instance = this;
-            DontDestroyOnLoad(gameObject);
+            XRTest,
+            DevTest,
         }
-        else
+
+        public void LoadScenarioBusAccident()
         {
-            Destroy(gameObject);
-        }
-    }
-
-    public void LoadScene(string sceneName)
-    {
-        UnityEngine.SceneManagement.SceneManager.LoadScene(sceneName);
-    }
-
-    public void UnloadScene(string sceneName)
-    {
-        UnityEngine.SceneManagement.SceneManager.UnloadSceneAsync(sceneName);
-    }
-
-    public async Task LoadSceneAsync(string sceneName)
-    {
-        try {
-            await Task.Run(() => UnityEngine.SceneManagement.SceneManager.LoadSceneAsync(sceneName));
-        }
-        catch (Exception e)
-        {
-            Debug.Log($"error : {e.Message}");
-        }
-    }
-
-    public async Task UnloadSceneAsync(string sceneName)
-    {   
-        try {
-            await Task.Run(() => UnityEngine.SceneManagement.SceneManager.UnloadSceneAsync(sceneName));
-        }
-        catch (Exception e)
-        {
-            Debug.Log($"error : {e.Message}");
-        }
-    }
-
-    public string GetActiveSceneName()
-    {
-        return UnityEngine.SceneManagement.SceneManager.GetActiveScene().name;
-    }
-
-    public bool IsSceneLoaded(string sceneName)
-    {
-        for (int i = 0; i < UnityEngine.SceneManagement.SceneManager.sceneCount; i++)
-        {
-            if (UnityEngine.SceneManagement.SceneManager.GetSceneAt(i).name == sceneName)
+            string targetSceneName = SceneName.XRTest.ToString();
+            if (IsSceneLoaded(targetSceneName))
             {
-                return true;
+                if (targetSceneName == GetActiveSceneName())
+                {
+                    Debug.LogError($"Error: Scene '{targetSceneName}' is already loaded.");
+                }
+                else
+                {
+                    Debug.LogError($"Error: Scene '{targetSceneName}' is already loaded but not active.");
+                }
+            }
+            else
+            {
+                UnityEngine.SceneManagement.SceneManager.LoadScene(targetSceneName);
             }
         }
-        return false;
-    }
 
-    public void TransitionToScene(string sceneName, float transitionTime){
-        // Add functionality
+        public void LoadMainMenu()
+        {
+            string targetSceneName = SceneName.DevTest.ToString();
+            if (IsSceneLoaded(targetSceneName))
+            {
+                if (targetSceneName == GetActiveSceneName())
+                {
+                    Debug.LogError($"Error: Scene '{targetSceneName}' is already loaded.");
+                }
+                else
+                {
+                    Debug.LogError($"Error: Scene '{targetSceneName}' is already loaded but not active.");
+                }
+            }
+            else
+            {
+                UnityEngine.SceneManagement.SceneManager.LoadScene(targetSceneName);
+            }
+        }
+
+        public string GetActiveSceneName()
+        {
+            return UnityEngine.SceneManagement.SceneManager.GetActiveScene().name.ToString();
+        }
+
+        public bool IsSceneLoaded(string sceneName)
+        {
+            for (int i = 0; i < UnityEngine.SceneManagement.SceneManager.sceneCount; i++)
+            {
+                if (UnityEngine.SceneManagement.SceneManager.GetSceneAt(i).name == sceneName)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
     }
 }
