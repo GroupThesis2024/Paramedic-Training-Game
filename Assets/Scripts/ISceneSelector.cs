783e8fbe774a42eacf99d020830b3282de9a7a32
Aki Peltokangas <v0peak00@students.oamk.fi>
2024-04-03 11:44:52 +0300

    refactor: Remove obsolete comments

--- Assets/Scripts/ISceneSelector.cs
+++ Assets/Scripts/ISceneSelector.cs
@@ -2,16 +2,12 @@ namespace SceneSelector
 {
     public interface ISceneSelector
     {
-        // Load explicit scene: Bus Accident Scene
         void LoadScenarioBusAccident();
 
-        // Load explicit scene: Main menu
         void LoadMainMenu();
 
-        // Get the name of the currently active scene
         string GetActiveSceneName();
 
-        // Check if a scene is loaded
         bool IsSceneLoaded(string sceneName);
     }
 }
