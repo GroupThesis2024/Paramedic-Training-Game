da0b105eab4fec6887cd8ce6a8d490c2f7b4513f
Aki Peltokangas <v0peak00@students.oamk.fi>
2024-04-08 10:22:30 +0300

    feat: Add PatientSpawnPoint script

---
+++ Assets/Scripts/Scene Bootstrapper/PatientSpawnPoint.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PatientSpawnPoint : MonoBehaviour
+{
+    public bool IsSitting;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
