4266b3bde775f3994e9992811566879d86f271e7
Aki Peltokangas <v0peak00@students.oamk.fi>
2024-04-09 14:30:52 +0300

    refactor: Introduce decoupling by relying on prefab

--- Assets/Scripts/Scene Bootstrapper/ISpawnPointAdjuster.cs
+++ Assets/Scripts/Scene Bootstrapper/ISpawnPointAdjuster.cs
@@ -2,5 +2,5 @@ using UnityEngine;
 
 public interface ISpawnPointAdjuster
 {
-    Vector3 AdjustSpawnPointForPrefab(GameObject prefab, Vector3 spawnPoint);
+    public float GetYOffsetForPrefab(GameObject prefab);
 }
