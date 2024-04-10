8c0f2a790d2f0f2f077b24b705f6f4bdc7c8ab40
Aki Peltokangas <v0peak00@students.oamk.fi>
2024-04-10 11:23:17 +0300

    refactor: Remove duplicate code

--- Assets/Scripts/Scene Bootstrapper/SpawnPointAdjuster.cs
+++ Assets/Scripts/Scene Bootstrapper/SpawnPointAdjuster.cs
@@ -17,41 +17,27 @@ public class SpawnPointAdjuster : ISpawnPointAdjuster
 
     private float GetPrefabHeight(GameObject prefab)
     {
-        Renderer[] renderers = prefab.GetComponentsInChildren<Renderer>();
-        if (renderers == null || renderers.Length == 0)
-        {
-            Debug.LogWarning("No renderers found in the prefab or its children.");
-            return 0f; // Return a default height
-        }
-
-        Bounds bounds = new Bounds();
-        foreach (Renderer renderer in renderers)
-        {
-            if (renderer != null && renderer.bounds.size != Vector3.zero)
-            {
-                bounds.Encapsulate(renderer.bounds);
-            }
-        }
-
-        if (bounds.size.y <= 0f)
-        {
-            Debug.LogWarning("Invalid height calculated for the prefab.");
-            return 0f; // Return a default height
-        }
-
+        Bounds bounds = GetCombinedBounds(prefab);
         return bounds.size.y;
     }
 
     private float GetVerticalPivotOffset(GameObject prefab)
     {
+        Bounds bounds = GetCombinedBounds(prefab);
+        // Calculate the vertical pivot offset
+        return (bounds.center.y - bounds.min.y) / bounds.size.y;
+    }
+
+    private Bounds GetCombinedBounds(GameObject prefab)
+    {
         Renderer[] renderers = prefab.GetComponentsInChildren<Renderer>();
         if (renderers == null || renderers.Length == 0)
         {
             Debug.LogWarning("No renderers found in the prefab or its children.");
-            return 0.5f; // Return the default pivot offset
+            return new Bounds(); // Return an empty bounds
         }
 
-        Bounds bounds = new Bounds();
+        Bounds bounds = new();
         foreach (Renderer renderer in renderers)
         {
             if (renderer != null && renderer.bounds.size != Vector3.zero)
@@ -59,8 +45,6 @@ public class SpawnPointAdjuster : ISpawnPointAdjuster
                 bounds.Encapsulate(renderer.bounds);
             }
         }
-
-        // Calculate the vertical pivot offset
-        return (bounds.center.y - bounds.min.y) / bounds.size.y;
+        return bounds;
     }
 }
