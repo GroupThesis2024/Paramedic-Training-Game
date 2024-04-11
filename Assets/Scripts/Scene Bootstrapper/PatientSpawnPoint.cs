c764e6a00cebd6fa989af530ff589eb5a918fac6
Aki Peltokangas <v0peak00@students.oamk.fi>
2024-04-11 11:59:26 +0300

    refactor: Remove bool isSitting and func GetHeightFromSurface()
    - As the prefabs for the project haven't been decided, keeping
        the bool isSitting is obsolete at this point.
    - Function GetHeightFromSurface() should be moved to SceneBootstrapper
        as it serves it's purpose better there.

--- Assets/Scripts/Scene Bootstrapper/PatientSpawnPoint.cs
+++ Assets/Scripts/Scene Bootstrapper/PatientSpawnPoint.cs
@@ -2,18 +2,5 @@ using UnityEngine;
 
 public class PatientSpawnPoint : MonoBehaviour
 {
-    public bool IsSitting;
-
-    public float GetHeightFromSurface()
-    {
-        RaycastHit hit;
-        if (Physics.Raycast(transform.position, -transform.up, out hit))
-        {
-            return hit.point.y - transform.position.y; // Negative ray cast result
-        }
-        else
-        {
-            return 0f; // Default offset if no collision is detected
-        }
-    }
+    
 }
