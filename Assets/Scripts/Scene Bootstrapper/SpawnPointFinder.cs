1114d514c7cad12514de86b197eabaa6bd96176f
Aki Peltokangas <v0peak00@students.oamk.fi>
2024-04-11 12:48:40 +0300

    refactor: Refactor class
    - Move GetRandomSpawnPoint() to SceneBootstrapper
    - Change FindSpawnPoints() to GetAllPatientSpawnPointsInScene() and changed
        return type to PatientSpawnPoint[]

--- Assets/Scripts/Scene Bootstrapper/SpawnPointFinder.cs
+++ Assets/Scripts/Scene Bootstrapper/SpawnPointFinder.cs
@@ -1,38 +1,15 @@
-using System.Collections.Generic;
 using UnityEngine;
 
 public class SpawnPointFinder
 {
-    private List<Transform> spawnPoints = new List<Transform>();
-    private List<Transform> clonedSpawnPoints;
-
     public SpawnPointFinder()
     {
-        FindSpawnPoints();
+        GetAllPatientSpawnPointsInScene()
     }
 
-    private void FindSpawnPoints()
+    public List<PatientSpawnPoint> GetAllPatientSpawnPointsInScene()
     {
-        GameObject[] spawnPointObjects = GameObject.FindGameObjectsWithTag("PatientSpawnPoint");
-        foreach (GameObject spawnPointObject in spawnPointObjects)
-        {
-            spawnPoints.Add(spawnPointObject.transform);
-        }
-        clonedSpawnPoints = new List<Transform>(spawnPoints);
-    }
-
-    public Transform GetRandomSpawnPoint()
-    {
-        if (spawnPoints.Count == 0)
-        {
-            Debug.LogError("No spawn points found.");
-            return null;
-        }
-
-        int randomIndex = Random.Range(0, clonedSpawnPoints.Count);
-        Transform spawnPoint = clonedSpawnPoints[randomIndex];
-        clonedSpawnPoints.RemoveAt(randomIndex);
-
-        return spawnPoint;
+        PatientSpawnPoint[] patientSpawnPoints = GameObject.FindGameObjectsWithTag("PatientSpawnPoint");
+        return patientSpawnPoints;
     }
 }
