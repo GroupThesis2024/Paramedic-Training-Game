4e44aa6ddfb65d592b22664823b401adf2d25495
Aki Peltokangas <v0peak00@students.oamk.fi>
2024-04-22 13:26:52 +0300

    refactor: Add TODO to PatientSpawnPoint class

--- Assets/Scripts/Scene Bootstrapper/PatientSpawnPoint.cs
+++ Assets/Scripts/Scene Bootstrapper/PatientSpawnPoint.cs
@@ -4,7 +4,7 @@ namespace LevelLoader
 {
     public class PatientSpawnPoint : MonoBehaviour
     {
-
+        // TODO: Empty on purpose, will be implemented in a later issue with functionality
     }
 
 }
