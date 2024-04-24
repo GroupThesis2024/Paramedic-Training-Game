277242d067a2ac296bdb6ac5c92b4cbc6d230097
diana-sy <diana.v.syvorotka@gmail.com>
2024-04-24 08:30:26 +0300

    Updated IPatient script
    Added methods from the class diagram to the script

--- Assets/Scripts/Game Core/IPatient.cs
+++ Assets/Scripts/Game Core/IPatient.cs
@@ -4,10 +4,40 @@ using System.Collections.Generic;
 
 public interface IPatient
 {
-	string GetName();
-	byte GetClassification();
-	byte SetClassification();
-	float GetPulse();
-	float GetPulseAtWrist();
-	float GetBreathingRate();
+  //ID   
+    string GetName(); 
+
+
+    //Condition 
+    string GetCondition(); 
+    string SetCondition(); 
+    bool GetIsDead(); 
+    bool SetIsDead (); 
+
+ 
+    //Heart Rate 
+    float GetPulse(); 
+    float SetPulse(); 
+    float GetPulseAtWrist(); 
+
+ 
+    //Breathing Rate 
+    float GetBreathingRate(); 
+    float SetBreathingRate(); 
+
+
+    //Bleeding 
+    bool getBleeding(); 
+    bool SetBleeding(); 
+
+
+    //Dialogue with NPC 
+    string GetConversation(); 
+	 string SetConversation(); 
+
+
+    //Classification 
+    byte GetClassification(); 
+    byte SetClassification(); 
+
 }
