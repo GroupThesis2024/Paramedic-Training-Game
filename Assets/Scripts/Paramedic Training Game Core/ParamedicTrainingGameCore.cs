aaa3501715d60dd9655986e0a90075a7f9093389
Aki Peltokangas <v0peak00@students.oamk.fi>
2024-05-10 13:03:05 +0300

    feat: Add EventHandlers
    - Subscribing to the PatientsCreated event of the PatientFactory
    - Subscribes EventHandlers given in parametrized constructor to PatientsInitializedSuccesfullyEvent
        and notifying subscribed eventhandlers when patients are successfully
        initialized with CustomArgs of instance reference to PatientFactory.

--- Assets/Scripts/Paramedic Training Game Core/ParamedicTrainingGameCore.cs
+++ Assets/Scripts/Paramedic Training Game Core/ParamedicTrainingGameCore.cs
@@ -1,18 +1,67 @@
-using System.Collections;
+using System;
 using System.Collections.Generic;
 
 
-public class ParamedicTrainingGameCore
+namespace Backend
 {
-	private List<IGameCoreEventListener> listeners;
-
-	public void addListener(IGameCoreEventListener listener)
+	public class ParamedicTrainingGameCore
 	{
-		listeners.Add(listener);
-	}
+		private readonly List<EventHandler<CustomEventArgs>> eventHandlers;
 
-	public void newGame()
-	{
-		// TODO
+		private PatientFactory patientFactory;
+
+		public event EventHandler<CustomEventArgs> PatientsInitializedSuccesfullyEvent;
+
+		public ParamedicTrainingGameCore(List<EventHandler<CustomEventArgs>> eventHandlers)
+		{
+			this.eventHandlers = eventHandlers;
+			SubscribeEventHandlersFromList();
+			SubscribeToPatientFactoryAndInitializePatients();
+			UnSubscribeEventHandlersFromList();
+		}
+
+		private void SubscribeEventHandlersFromList()
+		{
+			foreach (EventHandler<CustomEventArgs> eventHandler in eventHandlers)
+			{
+				PatientsInitializedSuccesfullyEvent += eventHandler;
+			}
+		}
+
+		private void SubscribeToPatientFactoryAndInitializePatients()
+		{
+			patientFactory = new PatientFactory();
+			patientFactory.PatientsCreated += OnPatientsCreated;
+			patientFactory.InitializePatients();
+			patientFactory.PatientsCreated -= OnPatientsCreated;
+		}
+
+		private void OnPatientsCreated(object sender, EventArgs e)
+		{
+			NotifySubscribersPatientsCreated();
+		}
+
+		private void NotifySubscribersPatientsCreated()
+		{
+			foreach (Delegate delegateMethod in PatientsInitializedSuccesfullyEvent.GetInvocationList())
+			{
+				try
+				{
+					delegateMethod.DynamicInvoke(this, new CustomEventArgs(patientFactory));
+				}
+				catch (Exception ex)
+				{
+					Console.WriteLine($"Exception caught: {ex.Message}");
+				}
+			}
+		}
+
+		private void UnSubscribeEventHandlersFromList()
+		{
+			foreach (EventHandler<CustomEventArgs> eventHandler in eventHandlers)
+			{
+				PatientsInitializedSuccesfullyEvent -= eventHandler;
+			}
+		}
 	}
 }
