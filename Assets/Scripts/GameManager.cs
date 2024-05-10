2f55db021305757bc08131ea390a8f3d43a96836
Aki Peltokangas <v0peak00@students.oamk.fi>
2024-05-10 13:18:37 +0300

    feat: Add field EventHandlers to GameManager
    - This List<EventHandler<CustomEventArgs>> eventHandlers is passed to
        ParamedicTrainingGameCore in constructor.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,14 +1,18 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics.Tracing;
 using UnityEngine;
+using Backend;
 
 public class GameManager : MonoBehaviour
 {
 	private ParamedicTrainingGameCore gameCore;
+	List<EventHandler<CustomEventArgs>> eventHandlers = new List<EventHandler<CustomEventArgs>>();
 
 	private void Awake()
 	{
-		gameCore = new ParamedicTrainingGameCore();
+		gameCore = new ParamedicTrainingGameCore(eventHandlers);
 	}
 
 	private void Start()
