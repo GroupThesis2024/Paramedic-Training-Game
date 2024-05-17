0a25a802490c22996d80d5f224d1761d1b3dcb43
Aki Peltokangas <v0peak00@students.oamk.fi>
2024-05-17 14:31:36 +0300

    feat: Add method GetAllPatients()
    - Add field List<PatientInformation> patients
    - Add method GetAllPatients() which returns patients list

--- Assets/Scripts/Paramedic Training Game Core/ParamedicTrainingGameCore.cs
+++ Assets/Scripts/Paramedic Training Game Core/ParamedicTrainingGameCore.cs
@@ -1,15 +1,22 @@
 using System.Collections.Generic;
-using UnityEngine;
 
 namespace Backend
 {
 	public class ParamedicTrainingGameCore
 	{
+		private List<PatientInformation> patients;
+
 		public ParamedicTrainingGameCore(List<IGameEventListener> listeners)
 		{
 			PatientFactory patientFactory = new PatientFactory();
 			patientFactory.InitializePatients();
+			patients = patientFactory.GetAllPatients();
 			InformEventListeners(listeners);
+		}
+
+		public List<PatientInformation> GetAllPatients()
+		{
+			return patients;
 		}
 
 		private void InformEventListeners(List<IGameEventListener> listeners)
